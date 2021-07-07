8c6c5a566e0fdbc00d6a852e02aaa22420f8b5bf
Jiyoung <thdwldud8@gmail.com>
2021-07-07 20:48:03 +0900

    2021-07-07

---
+++ python-100/6066.py
@@ -0,0 +1,18 @@
+#3개의 정수(a, b, c)가 입력되었을 때, 짝(even)/홀(odd)을 출력해보자.
+
+a,b,c = input().split()
+
+if int(a)%2 == 0:
+    print('even')
+else:
+    print('odd')
+
+if int(b)%2 == 0:
+    print('even')
+else:
+    print('odd')
+
+if int(c)%2 == 0:
+    print('even')
+else:
+    print('odd')    
