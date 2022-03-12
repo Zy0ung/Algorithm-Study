d2d5a38767a50f846cb0d5d1936ad39c1b5ea92d
Jiyoung <thdwldud8@gmail.com>
2022-03-12 20:51:59 +0900

    2022-03-12

--- Baekjoon/1924.py
+++ Baekjoon/1924.py
@@ -1,14 +1,11 @@
-#월에 따른 일 수
+x, y = map(int, input().split())
+
+day = 0
 A = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-#요일
-B = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-#요일을 계산하기 위한 변수
-DAY = 0
+B = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
-X, Y = map(int, input().split())
+for i in range(x-1):
+    day += A[i]
+day = (day+y) %7
 
-for i in range(X-1):
-    DAY = DAY + A[i]
-DAY = (DAY + Y) % 7
-
-print(B[DAY])
+print(B[day])
