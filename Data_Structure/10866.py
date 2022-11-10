ecd504f3f841e9a207e21db8848220f88aa98d54
Jiyoung <thdwldud8@gmail.com>
2022-11-10 18:45:05 +0900

    10866

---
+++ Data_Structure/10866.py
@@ -0,0 +1,50 @@
+from collections import deque
+import sys
+
+N = int(input())
+
+
+dq = deque()
+
+
+for i in range(N):
+    cmd = list(sys.stdin.readline().split())
+
+    if cmd[0] == 'push_front':
+        dq.appendleft(cmd[1])
+    
+    elif cmd[0] == 'push_back':
+        dq.append(cmd[1])
+
+    elif cmd[0] == 'pop_front':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq.popleft())
+    
+    elif cmd[0] == 'pop_back':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq.pop())
+
+    elif cmd[0] == 'size':
+        print(len(dq))
+
+    elif cmd[0] == 'empty':
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif cmd[0] == 'front':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[0])
+
+    elif cmd[0] == 'back':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[-1])
